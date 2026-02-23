85acffde329e92335024778d0652400b902a56b2
Tchoua Tchatouo <b.tchoua.tchatouo@accenture.com>
2026-02-23 16:02:09 +0400

    alternate between viewing answered and unanswered polls

--- employee-polls/src/components/Home.js
+++ employee-polls/src/components/Home.js
@@ -1,38 +1,59 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { useSelector } from "react-redux";
 import PollCard from "./PollCard";
 import { formatDate } from "../utils/helpers";
 
-const Home = () => {
+export default function Home() {
+  const [tab, setTab] = useState("unanswered"); 
+
   const authedUser = useSelector((s) => s.authedUser);
-  const users = useSelector((s) => s.users);
-  const questions = useSelector((s) => s.questions);
+  const users = useSelector((s) => s.users || {});
+  const questions = useSelector((s) => s.questions || {});
 
   const answeredIds = useMemo(() => {
     const answers = users?.[authedUser]?.answers || {};
     return new Set(Object.keys(answers));
   }, [users, authedUser]);
 
-  const sorted = useMemo(() => {
-    return Object.values(questions || {}).sort((a, b) => b.timestamp - a.timestamp);
+  const sortedQuestions = useMemo(() => {
+    return Object.values(questions).sort((a, b) => b.timestamp - a.timestamp);
   }, [questions]);
 
-  const newQuestions = useMemo(() => {
-    return sorted.filter((q) => !answeredIds.has(q.id));
-  }, [sorted, answeredIds]);
+  const unanswered = useMemo(() => {
+    return sortedQuestions.filter((q) => !answeredIds.has(q.id));
+  }, [sortedQuestions, answeredIds]);
 
-  const doneQuestions = useMemo(() => {
-    return sorted.filter((q) => answeredIds.has(q.id));
-  }, [sorted, answeredIds]);
+  const answered = useMemo(() => {
+    return sortedQuestions.filter((q) => answeredIds.has(q.id));
+  }, [sortedQuestions, answeredIds]);
+
+  const list = tab === "unanswered" ? unanswered : answered;
 
   return (
     <div className="dash-wrap">
-      {/* New Questions */}
+      <div className="dash-toggle">
+        <button
+          className={tab === "unanswered" ? "dash-tab active" : "dash-tab"}
+          onClick={() => setTab("unanswered")}
+        >
+          Unanswered Questions
+        </button>
+
+        <button
+          className={tab === "answered" ? "dash-tab active" : "dash-tab"}
+          onClick={() => setTab("answered")}
+        >
+          Answered Questions
+        </button>
+      </div>
+
       <section className="dash-panel">
-        <div className="dash-panel-header">New</div>
+        <div className="dash-panel-header">
+          {tab === "unanswered" ? "New Questions" : "Done"}
+        </div>
 
         <div className="dash-grid">
-          {newQuestions.map((q) => (
+          {list.map((q) => (
             <PollCard
               key={q.id}
               questionId={q.id}
@@ -43,23 +64,6 @@ const Home = () => {
         </div>
       </section>
 
-      {/* Done */}
-      <section className="dash-panel">
-        <div className="dash-panel-header">Done</div>
-
-        <div className="dash-grid">
-          {doneQuestions.map((q) => (
-            <PollCard
-              key={q.id}
-              questionId={q.id}
-              authorName={users?.[q.author]?.id || q.author}
-              timeText={formatDate(q.timestamp)}
-            />
-          ))}
-        </div>
-      </section>
     </div>
   );
-};
-
-export default Home;
+}
